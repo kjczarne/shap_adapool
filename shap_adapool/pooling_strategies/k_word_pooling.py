c976dcd3f374b3946b77b279f7c05b386be07dce
John Kim <70460039+johnjeihokim@users.noreply.github.com>
2024-02-24 14:07:12 -0500

    K word implementation (#1)
    * buggy implementation
    
    * bugfix
    
    * input funciton for k
    
    * requested changes
    
    * removing comments
    
    ---------
    
    Approved-by: Krzysztof <30905054+kjczarne@users.noreply.github.com>

--- shap_adapool/pooling_strategies/k_word_pooling.py
+++ shap_adapool/pooling_strategies/k_word_pooling.py
@@ -1,5 +1,61 @@
+import shap
+import pickle
+import numpy as np
+import matplotlib
+import matplotlib.pyplot as plt
+from numpy.typing import NDArray, ArrayLike
+from typing import Callable, Iterable, Any
+from functools import reduce, partial
+from io import StringIO
+import regex_spm
+import argparse
+
+from ..initializer import init
+from ..pooler import shap_value_pooler, two_element_sum
+from ..token_concatenation import add_strings, k_word_concat
+from ..plotting import save_plot
+
+
 def main():
-    raise NotImplementedError()
+    parser = argparse.ArgumentParser(description='Script with integer argument.')
+
+    parser.add_argument('k', type=int, help='k_value')
+
+    args = parser.parse_args()
+
+    k = args.k
+    if k <= 0:
+        raise ValueError("positive integer k required")
+
+
+    init()
+    # importing the shapley values from a pickle file to save some time
+
+    with open('shap.pkl', 'rb') as f:
+        shap_values = pickle.load(f)
+
+
+    phrases, phrase_indices = k_word_concat(shap_values.data[0], k)
+
+    values = shap_value_pooler(shap_values.values[0],
+                               phrase_indices,
+                               two_element_sum, yieldLast=True)
+
+    # shap.plots.text(shap_values=)
+    exp = shap._explanation.Explanation(values=np.array(list(values))[None, :],  # need to add batch dimension
+                                        base_values=np.array([shap_values.base_values[0]]),
+                                        data=((list(phrases),)))  # a 1-element tuple
+
+
+    print(exp)
+    single_sample_plotting_functions = [
+        ("text", partial(shap.plots.text, display=False)),
+    ]
+
+    SAMPLE_IDX = 0
+    for name, f in single_sample_plotting_functions:
+        plot = f(shap_values=exp)
+        save_plot(plot, name + "_pooling")
 
 
 if __name__ == "__main__":
--- shap_adapool/token_concatenation.py
+++ shap_adapool/token_concatenation.py
@@ -65,3 +65,24 @@ def sentence_concat(array: NDArray[TokenDtype]) -> (NDArray[PhraseDtype], NDArra
         return True
 
     return partial(token_concat, write_condition=_write_condition, end_condition=_end_condition)(array)
+
+
+def k_word_concat(array: NDArray[TokenDtype], k: int) -> (NDArray[PhraseDtype], NDArray[IdxDtype]):
+    sentences = []
+    token_buffer = StringIO()
+    index_map = []
+    idx = 0
+    i = 1
+    arrayLength = len(array)
+    for token in array:
+        index_map.append(idx)
+        if i % k == 0 or i == arrayLength:
+            token_buffer.write(token)
+            sentences.append(token_buffer.getvalue())
+            token_buffer = StringIO()  # reset buffer
+            idx += 1
+        else:
+            token_buffer.write(token)
+        i += 1
+    index_map.append(-1)  # indicates the end of the array
+    return sentences, index_map

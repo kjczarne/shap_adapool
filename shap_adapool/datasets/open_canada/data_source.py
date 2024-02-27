1adf05678b6718374c1673f5dfd806275f92c1a0
Chris Czarnecki <cczarnec@uwaterloo.ca>
2024-02-26 22:48:19 -0500

    Abstract away path to data, add `__init__` file for the classifier subpackage

---
+++ shap_adapool/datasets/open_canada/data_source.py
@@ -0,0 +1,3 @@
+from pathlib import Path
+
+DATASET_PATH = Path("2023_24_grants_and_contributions.csv")
--- shap_adapool/datasets/open_canada/get_data.py
+++ shap_adapool/datasets/open_canada/get_data.py
@@ -1,6 +1,8 @@
 import pandas as pd
 from pathlib import Path
 
-def get_data(path: Path = Path("2023_24_grants_and_contributions.csv")) -> pd.DataFrame:
+from .data_source import DATASET_PATH
+
+def get_data(path: Path = DATASET_PATH) -> pd.DataFrame:
     """Loads the Open Canada dataset from a CSV file."""
     return pd.read_csv(path, header=0)

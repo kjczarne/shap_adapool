6a8985e8f1e64941b25b6c6928710ba70b24a813
Chris Czarnecki <cczarnec@uwaterloo.ca>
2024-03-28 10:37:15 -0400

    Add a `main` function to the `plotting` module

--- shap_adapool/plotting.py
+++ shap_adapool/plotting.py
@@ -1,5 +1,9 @@
 import matplotlib
 import matplotlib.pyplot as plt
+import pickle
+import shap
+import argparse
+from pathlib import Path
 
 
 def save_plot(plot: matplotlib.figure.Figure | str,
@@ -15,3 +19,23 @@ def save_plot(plot: matplotlib.figure.Figure | str,
             plot.savefig(f"{directory}/shap_plot_{name}.png")
         case _:
             raise ValueError(f"Unknown plot type {type(plot)}")
+
+
+def main():
+    parser = argparse.ArgumentParser("Create a SHAP plot from stored SHAP values.")
+    parser.add_argument("-p", "--path", type=str, help="Path to the shap_values.pkl file",
+                        default="results/shap_values.pkl")
+
+    args = parser.parse_args()
+    path = Path(args.path)
+
+    with open(path, "rb") as f:
+        shap_values = pickle.load(f)
+
+    # Optional: save a veeeery big original plot collection:
+    plot = shap.plots.text(shap_values, display=False)
+    save_plot(plot, "shap_plot_mistral")
+
+
+if __name__ == "__main__":
+    main()

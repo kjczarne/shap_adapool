1a8b099845470a37fed9b04d97b7691668eb4923
Chris Czarnecki <cczarnec@uwaterloo.ca>
2024-05-14 10:33:34 -0400

    Change the dataset path for debugging

--- shap_adapool/datasets/companies/data_source.py
+++ shap_adapool/datasets/companies/data_source.py
@@ -1,5 +1,6 @@
 from pathlib import Path
 
-DATASET_PATH = Path("D:\Dataset\Tech-2020-02-12.csv")
+# DATASET_PATH = Path("D:\Dataset\Tech-2020-02-12.csv")
+DATASET_PATH = Path("Public Large Firms Example.csv")
 CLEAN_DATASET_PATH = Path("results/public.csv")
 DATASET_OUTPUT_PATH = Path("results/public")
